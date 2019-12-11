3c2943d82c47877a042b99ef1eef0c27629ec2a7
hatharry <hatharry@hotmail.com>
2019-12-11 23:07:12 +1300

    Create runme.sh

---
+++ runme.sh
@@ -0,0 +1,13 @@
+mkdir build64
+cd build64
+cmake -DTARGET_ARCH=x86_64-w64-mingw32 -G Ninja ..
+ninja gcc
+ninja mpv
+cd ..
+
+mkdir build32
+cd build32
+cmake -DTARGET_ARCH=i686-w64-mingw32 -G Ninja ..
+ninja gcc
+ninja mpv
+cd ..
