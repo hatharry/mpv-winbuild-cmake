847ea44c310853c49a3ab5f090b49bbf0e747997
hatharry <hatharry@hotmail.com>
2020-12-29 21:48:01 +1300

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,14 +1,6 @@
-set -e
-mkdir build64
-cd build64
-cmake -DTARGET_ARCH=x86_64-w64-mingw32 -G Ninja ..
-ninja gcc
-ninja mpv
-cd ..
-
-mkdir build32
-cd build32
-cmake -DTARGET_ARCH=i686-w64-mingw32 -G Ninja ..
+mkdir build-$1
+cd build-$1
+cmake -DTARGET_ARCH=$1-w64-mingw32 -G Ninja ..
 ninja gcc
 ninja mpv
 cd ..
